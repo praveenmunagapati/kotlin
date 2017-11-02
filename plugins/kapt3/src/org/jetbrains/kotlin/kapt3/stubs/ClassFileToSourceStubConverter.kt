7588ba8658cda4efa7811f8bcb34cb09dd1efb45
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-11-02 09:47:59 +0900

    Minor: Remove unused fileManager property

--- plugins/kapt3/src/org/jetbrains/kotlin/kapt3/stubs/ClassFileToSourceStubConverter.kt
+++ plugins/kapt3/src/org/jetbrains/kotlin/kapt3/stubs/ClassFileToSourceStubConverter.kt
@@ -46,7 +46,6 @@ import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.tree.*
 import javax.lang.model.element.ElementKind
-import javax.tools.JavaFileManager
 import com.sun.tools.javac.util.List as JavacList
 
 class ClassFileToSourceStubConverter(
@@ -87,8 +86,6 @@ class ClassFileToSourceStubConverter(
 
     val bindings: Map<String, KaptJavaFileObject>
         get() = _bindings
-
-    private val fileManager = kaptContext.context.get(JavaFileManager::class.java) as JavacFileManager
 
     val treeMaker = TreeMaker.instance(kaptContext.context) as KaptTreeMaker
 
