559dc865fda3b9bc261ba9ed873b43de470698b6
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-11-02 12:39:49 +0100

    Convert all compiler modules to intellij plugin

--- compiler/daemon/daemon-common/build.gradle.kts
+++ compiler/daemon/daemon-common/build.gradle.kts
@@ -3,12 +3,22 @@ apply { plugin("kotlin") }
 
 jvmTarget = "1.6"
 
+configureIntellijPlugin {
+    setExtraDependencies("intellij-core")
+}
+
 dependencies {
     compile(project(":core"))
     compile(project(":compiler:util"))
     compile(project(":compiler:cli-common"))
-    compile(ideaSdkCoreDeps(*(rootProject.extra["ideaCoreSdkJars"] as Array<String>)))
     compile(project(":kotlin-stdlib"))
+}
+
+afterEvaluate {
+    dependencies {
+        compile(intellijCoreJar())
+        compile(intellijCoreJarDependencies())
+    }
 }
 
 sourceSets {
