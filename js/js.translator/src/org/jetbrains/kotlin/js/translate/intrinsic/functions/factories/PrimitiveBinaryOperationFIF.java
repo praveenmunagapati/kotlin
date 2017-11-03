bdeeffecb4b2975a310512aedb4505c1c220f489
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-11-03 17:21:20 +0300

    ~fixup

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/intrinsic/functions/factories/PrimitiveBinaryOperationFIF.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/intrinsic/functions/factories/PrimitiveBinaryOperationFIF.java
@@ -196,18 +196,13 @@ public enum PrimitiveBinaryOperationFIF implements FunctionIntrinsicFactory {
                 return JsBinaryOperator.BIT_OR;
             case "and":
                 return JsBinaryOperator.BIT_AND;
+            case "xor":
+                return JsBinaryOperator.BIT_XOR;
             default:
                 break;
         }
 
         KtToken token = OperatorConventions.BINARY_OPERATION_NAMES.inverse().get(descriptorName);
-        if (token == null) {
-            token = OperatorConventions.BOOLEAN_OPERATIONS.inverse().get(descriptorName);
-        }
-        if (token == null) {
-            assert descriptorName.asString().equals("xor");
-            return JsBinaryOperator.BIT_XOR;
-        }
         return OperatorTable.getBinaryOperator(token);
     }
 
