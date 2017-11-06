6ef96b7dac1176886cd2a62a050a9cdb7a201b26
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-11-06 17:55:01 +0100

    Split core into descriptors, descriptors.jvm, deserialization, descriptors.runtime

---
+++ core/descriptors.runtime/build.gradle.kts
@@ -0,0 +1,21 @@
+apply { plugin("kotlin") }
+
+jvmTarget = "1.6"
+javaHome = rootProject.extra["JDK_16"] as String
+
+dependencies {
+    val compile by configurations
+    compile(project(":core:util.runtime"))
+    compile(project(":core:descriptors"))
+    compile(project(":core:descriptors.jvm"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.6"
+    targetCompatibility = "1.6"
+}
